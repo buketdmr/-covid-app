a04b51f2c8ea0eeda364f97ed07c48bd28b14930
BuketD <buket.demir@mqube.com>
2020-11-02 03:52:41 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./App.css";
+import { Home } from "./components/Home";
+import { About } from "./components/About";
+import { NoMatch } from "./components/NoMatch";
+import { Layout } from "./components/Layout";
+import { NavigationBar } from "./components/NavigationBar";
+import { CountryData } from "./components/CountryData";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <Router>
+        <NavigationBar />
+        <Layout>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/about" component={About} />
+            <Route exact path="/country/:id" component={CountryData} />
+            <Route component={NoMatch} />
+          </Switch>
+        </Layout>
+      </Router>
+    </React.Fragment>
   );
 }
 
